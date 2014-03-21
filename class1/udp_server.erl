809abbb203acc2cf19a70762afe78fccb9e21ad5
Ephemera <obliviscence+git@gmail.com>
2014-03-21 20:56:36 +0900

    Need space :telescope:

--- class1/udp_server.erl
+++ class1/udp_server.erl
@@ -9,7 +9,7 @@ main(_) ->
 loop(Socket) ->
   case gen_udp:recv(Socket, 0) of
     {ok, {Address, Port, Packet}} ->
-      io:format("~w:~w >> ~w~n",[Address, Port, binary_to_atom(Packet, utf8)]),
+      io:format("~w:~w >> ~w~n", [Address, Port, binary_to_atom(Packet, utf8)]),
       loop(Socket);
     {error, Reason} ->
       io:format(Reason),
