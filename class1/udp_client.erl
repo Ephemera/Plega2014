4b06b1fa4caf9837dbbcf9dc542392e87d45caaf
Ephemera <obliviscence+git@gmail.com>
2014-03-21 20:57:15 +0900

    Add socket close :closed_book:

--- class1/udp_client.erl
+++ class1/udp_client.erl
@@ -3,4 +3,5 @@
 main(Buf) ->
   PORT = 9999,
   {ok, Socket} = gen_udp:open(0, [binary, {broadcast, true}]),
-  gen_udp:send(Socket, {255,255,255,255}, PORT, Buf).
+  gen_udp:send(Socket, {255,255,255,255}, PORT, Buf),
+  gen_udp:close(Socket). 
